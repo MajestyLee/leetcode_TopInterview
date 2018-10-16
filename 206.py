9d905206188d972426c5b8cadc560e4e035ba075
MajestyLee <binjie_lee@163.com>
2018-10-15 21:02:56 -0400

    leetcode top interview1

--- 206.py
+++ 206.py
@@ -37,14 +37,12 @@ class Solution(object):
 
 # recursion
 class Solution:
-# @param {ListNode} head
-# @return {ListNode}
-def reverseList(self, head):
-    return self._reverse(head)
+    def reverseList(self, head):
+        return self._reverse(head)
 
-def _reverse(self, node, prev=None):
-    if not node:
-        return prev
-    n = node.next
-    node.next = prev
-    return self._reverse(n, node)
+    def _reverse(self, node, prev=None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        return self._reverse(n, node)
