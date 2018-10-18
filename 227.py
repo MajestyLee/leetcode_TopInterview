af36fafa22eddfa07d593da01372b641d36508f3
MajestyLee <binjie_lee@163.com>
2018-10-18 17:39:36 -0400

    leetcode top interview 10/18

---
+++ 227.py
@@ -0,0 +1,61 @@
+'''
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+
+Example 1:
+
+Input: "3+2*2"
+Output: 7
+Example 2:
+
+Input: " 3/2 "
+Output: 1
+Example 3:
+
+Input: " 3+5 / 2 "
+Output: 5
+Note:
+
+You may assume that the given expression is always valid.
+Do not use the eval built-in library function.
+'''
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # function to change string to int, and move i
+        def nextint(s, i):
+            res = 0
+            while i < len(s) and s[i] not in '+-*/':
+                res = res * 10 + ord(s[i]) - ord('0')
+                i += 1
+            return (res, i)
+
+        # init
+        s = s.replace(' ', '')
+        stack = []
+        i = 0
+        tmp, i = nextint(s, 0)
+        stack.append(tmp)
+        # push in stack
+        while i < len(s):
+            op = s[i]
+            tmp, i = nextint(s, i + 1)
+
+            if op == '+':
+                stack.append(tmp)
+            elif op == '-':
+                stack.append(-tmp)
+            elif op == '*':
+                stack[-1] *= tmp
+            elif op == '/':
+                stack[-1] = stack[-1] // tmp if stack[-1] >= 0 else -((-stack[-1]) // tmp)
+        # pop the stack
+        res = 0
+        for num in stack:
+            res += num
+        return res
