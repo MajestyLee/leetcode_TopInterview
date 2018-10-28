0906369fa28ded772c0c7cee23120c6ec2c036c3
MajestyLee <binjie_lee@163.com>
2018-10-28 18:34:17 -0400

    leetcode top interview

---
+++ 932.py
@@ -0,0 +1,54 @@
+'''
+Beautiful Array
+For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N, such that:
+
+For every i < j, there is no k with i < k < j such that A[k] * 2 = A[i] + A[j].
+
+Given N, return any beautiful array A.  (It is guaranteed that one exists.)
+
+
+
+Example 1:
+
+Input: 4
+Output: [2,1,4,3]
+Example 2:
+
+Input: 5
+Output: [3,1,2,5,4]
+
+
+Note:
+
+1 <= N <= 1000
+
+Explanation
+
+Another way is to divide into odds part and evens part.
+So there is no k with A[k] * 2 = odd + even
+1. Deletion
+Easy to prove.
+
+2. Addition
+If we have A[k] * 2 != A[i] + A[j],
+(A[k] + x) * 2 = A[k] * 2 + 2x != A[i] + A[j] + 2x = (A[i] + x) + (A[j] + x)
+
+E.g: [1,3,2] + 1 = [2,4,3].
+
+3. Multiplication
+If we have A[k] * 2 != A[i] + A[j],
+(A[k] * x) * 2 = A[k] * 2 * x != (A[i] + A[j]) * x = (A[i] * x) + (A[j] * x)
+
+E.g: [1,3,2] * 2 = [2,6,4]
+
+'''
+class Solution(object):
+    def beautifulArray(self, N):
+        """
+        :type N: int
+        :rtype: List[int]
+        """
+        l = [1]
+        while len(l) < N:
+            l = [i*2-1 for i in l]+ [i*2 for i in l]
+        return [i for i in l if i <= N]
